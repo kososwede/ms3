b84648f904116a545e1c33cf416a62dee9205f3f
Christopher McIlgrew <chrismcilgrew@icloud.com>
2019-12-02 10:38:06 +0000

    added about, contact htmls. added css to navbar

--- app.py
+++ app.py
@@ -67,6 +67,20 @@ def attractions():
 def home ():
     return render_template("index.html")
 
+
+@app.route("/about.html")
+def about ():
+    return render_template("about.html")
+
+
+@app.route("/contact.html")
+def contact ():
+    return render_template("contact.html")
+
+
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
