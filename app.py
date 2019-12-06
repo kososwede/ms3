f128b4ae34ef1cca00ce9483751cf85bca8e8822
Christopher McIlgrew <chrismcilgrew@icloud.com>
2019-12-06 11:54:09 +0000

    deleted config vars

--- app.py
+++ app.py
@@ -83,6 +83,6 @@ def contact():
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv("IP", "0.0.0.0"),
+    app.run(host=os.getenv("IP", "0.0.0.0"), 
     port=int(os.getenv("PORT", 5000)),debug=
     False)
