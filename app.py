32ae7b1fa278132db8a01a3cff2a7c0b5829192e
Christopher McIlgrew <chrismcilgrew@icloud.com>
2019-12-11 12:19:19 +0000

    added images to carousel in images page added an url validator in add place page commented on code and beautyfied the code

--- app.py
+++ app.py
@@ -16,84 +16,85 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
-
+# Function to load HOME page
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("index.html")
 
-
+# Function to load the Places page where you choose a category
 @app.route("/get_places")
 def get_places():
     return render_template("places.html", places=mongo.db.places.find().sort(
         "category_name"), categories=mongo.db.categories.find())
 
-
+# This is the ADD PLACE function where you can add a new place to the database
 @app.route("/add_place")
 def add_place():
     return render_template("addplace.html", categories=mongo.db.categories.find())
 
-
+#This function inserts (posts) the data that the user entered into my database
 @app.route("/insert_place", methods=['POST'])
 def insert_place():
     places = mongo.db.places
     places.insert_one(request.form.to_dict())
     return redirect(url_for('get_places'))
 
-
+# This function loads all of the PLACES in the MUSEUM category
 @app.route("/museums", methods=['GET'])
 def museums():
     print("got here")
     return render_template("museums.html", places=mongo.db.places.find({
         'category_name': 'Museums'}))
 
-
+# This function loads all of the PLACES in the PARKS category
 @app.route("/parks")
 def parks():
     return render_template("parks.html", places=mongo.db.places.find({
         'category_name': 'Parks'}))
 
-
+# This function loads all of the PLACES in the ACTIVITIES category
 @app.route("/activities")
 def activities():
     return render_template("activities.html", places=mongo.db.places.find({
         'category_name': 'Activities'}))
 
-
+# This function loads all of the PLACES in the FOOD & DRINK category
 @app.route("/fooddrink")
 def fooddrink():
     return render_template("fooddrink.html", places=mongo.db.places.find({
         'category_name': 'Food & Drink'}))
 
-
+# This function loads all of the PLACES in the HISTORY category
 @app.route("/history")
 def history():
     return render_template("history.html", places=mongo.db.places.find({
         'category_name': 'History'}))
 
-
+# This function loads all of the PLACES in the ATTRACTIONS category
 @app.route("/attractions")
 def attractions():
     return render_template("attractions.html", places=mongo.db.places.find({
         'category_name': 'Attractions'}))
 
-
+# This function loads the ABOUT page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-
+# This function loads the IMAGES page
 @app.route("/images")
 def images():
     return render_template("images.html")
 
+# This function loads EDIT place page
 @app.route('/edit_place/<place_id>')
 def edit_place(place_id):
     the_place = mongo.db.places.find_one({'_id': ObjectId(place_id)})
     all_categories = mongo.db.categories.find()
     return render_template('editplace.html', place=the_place, categories=all_categories)
 
-
+# This function UPDATES the the database 
 @app.route('/update_place/<place_id>', methods=['POST'])
 def update_place(place_id):
     places = mongo.db.places
@@ -107,7 +108,7 @@ def update_place(place_id):
     })
     return redirect(url_for('get_places'))
 
-
+# This function DELETES a record 
 @app.route('/delete_place/<place_id>')
 def delete_place(place_id):
     mongo.db.places.remove({'_id': ObjectId(place_id)})
